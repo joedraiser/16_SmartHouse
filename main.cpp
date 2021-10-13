c6b989d7c048f06080a7fe82f726441170621f92
Evgenii.Korchagov <joedraiser@gmail.com>
2021-10-13 10:28:52 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+
+enum states
+{
+    MAIN_POWER=1,
+    OUTLETS=2,
+    INDOOR_LIGHT=4,
+    OUTDOOR_LIGHT=8,
+    HEATING=16,
+    CONDITIONING=32
+};
+
+int main()
+{
+    int outsideTemp, insideTemp, isMovementDetected, isLightOn;
+    std::stringstream stream;
+    std::string input;
+
+    for(int hour=0;hour<24;hour++)
+    {
+
+        std::cout << "Input " << hour << ":00 environment state: ";
+        std::getline(std::cin, input);
+        stream << input;
+        stream >> outsideTemp >> input >> insideTemp >> input >> isMovementDetected >> input >> isLightOn;
+        
+    }
+    return 0;
+}
